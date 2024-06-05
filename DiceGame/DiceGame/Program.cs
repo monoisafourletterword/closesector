face7b48061825724643739f7a57dfd6cf26bec3
Никита Молчанов <107469981+monoisafourletterword@users.noreply.github.com>
2024-06-05 16:58:51 +0500

    Update Program.cs

--- DiceGame/DiceGame/Program.cs
+++ DiceGame/DiceGame/Program.cs
@@ -1,19 +1,16 @@
-﻿using System;
+using System;
 using System.Windows.Forms;
 
 namespace DiceGame
 {
     static class Program
     {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new DiceGameForm());
         }
     }
 }
